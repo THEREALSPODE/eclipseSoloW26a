00bcf75340da5ef0ebe29c4cd87cd118709066e0
THEREALSPODE <lucasaprawdzik@gmail.com>
2026-02-16 01:30:30 -0500

    programmer 2 moment

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello github partner project");
 		System.out.println("print line");
+		System.out.println("programmer 2 did this");
 	}
 
 }
